bd15fe794fca2198018c0b69d9c10fba64868b5b
shingooom <sng.med@gmail.com>
2023-08-02 14:57:17 +0900

    ユーザー登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  #before_action :basic_auth
+
+  #private
+
+  #def basic_auth
+    #authenticate_or_request_with_http_basic do |username, password|
+     # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+
+    #end
+ # end
+  
+ before_action :configure_permitted_parameters, if: :devise_controller?
+
+ protected
+
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(
+     :sign_up, keys: [:nickname, :"birthday(1i)", :"birthday(2i)", :"birthday(3i)", :first_name, :last_name, :first_name_kana, :last_name_kana]
+   )
+ end
 
 
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters.' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters.' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+  validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers.'
 end
